88d18ae2cce7efa4b56954be5445ba10c04dc39d
Clara Bringer <Clara Bringer>
2016-02-17 11:57:17 +0100

    Ecriture de la doc de Team et Player

--- src/main/java/org/copinf/cc/model/Player.java
+++ src/main/java/org/copinf/cc/model/Player.java
@@ -4,15 +4,25 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class Player {
-
+	
+	/** The zones where the pawns of the current player are at the beginning of the game */
 	private final Set<BoardZone> initialZones;
+	/** The name of the current player */
 	private final String name;
-
+	
+	/**
+	 * Constructs the player
+	 * @param name The name of the player
+	 */
 	public Player(final String name) {
 		this.name = name;
 		initialZones = new HashSet<>();
 	}
-
+	
+	/**
+	 * Returns if the current player has won
+	 * @return true if the current player has won
+	 */
 	public boolean hasWon() {
 		Set<BoardZone> opponentZones = new HashSet<BoardZone> ();
 		for (BoardZone bz : initialZones)
@@ -23,15 +33,28 @@ public class Player {
 		}
 		return true;
 	}
-
+	
+	/**
+	 * Returns the name of the current player
+	 * @return the name of the current player
+	 */
 	public String getName() {
 		return name;
 	}
-
+	
+	/**
+	 * Adds a zone to the initial zones of the current player
+	 * @param zone The new zone
+	 * @return false if the adding process failed
+	 */
 	public boolean addInitialZone(final BoardZone zone) {
 		return initialZones.add(zone);
 	}
-
+	
+	/**
+	 * Returns the initial zones of the current player
+	 * @return the initial zones of the current player
+	 */
 	public Set<BoardZone> getInitialZones() {
 		return initialZones;
 	}
--- src/main/java/org/copinf/cc/model/Team.java
+++ src/main/java/org/copinf/cc/model/Team.java
@@ -4,30 +4,57 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class Team {
-
+	
+	/** The opponent team */
 	private Team opponentTeam;
+	/** The players of the current team */
 	private Set<Player> players;
-
+	
+	/**
+	 * Constructs a new team
+	 */
 	public Team() {
 		this(new HashSet<>());
 	}
-
+	
+	/**
+	 * Constructs a new team
+	 * @param players The players in the team
+	 */
 	public Team(final Set<Player> players) {
 		this.players = players;
 	}
-
+	
+	/**
+	 * /**
+	 * Adds a player to players
+	 * @param player The player to add
+	 * @return false if the adding process failed
+	 */
 	public boolean addPlayer(final Player player) {
 		return players.add(player);
 	}
-
+	
+	/**
+	 * Returns the players of the current team
+	 * @return the players of the current team
+	 */
 	public Set<Player> getPlayers() {
 		return players;
 	}
-
+	
+	/**
+	 * Sets the opponent team
+	 * @param team
+	 */
 	public void setOpponentTeam(final Team team) {
 		opponentTeam = team;
 	}
-
+	
+	/**
+	 * Returns the opponent team
+	 * @return the opponent team
+	 */
 	public Team getOpponentTeam() {
 		return opponentTeam;
 	}
