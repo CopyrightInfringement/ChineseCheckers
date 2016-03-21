8bfd7e8a37f6785a0cadbe613770d38f1af5388c
Antonin Décimo <antonin.decimo@gmail.com>
2016-03-21 20:39:03 +0100

    Add client prototype

--- src/main/java/org/copinf/cc/net/client/Client.java
+++ src/main/java/org/copinf/cc/net/client/Client.java
@@ -1,6 +1,5 @@
-package org.copinf.cc.net.server;
+package org.copinf.cc.net.client;
 
-import org.copinf.cc.model.Player;
 import org.copinf.cc.net.Request;
 
 import java.io.IOException;
@@ -8,20 +7,18 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
 
-public class ClientThread extends Thread {
-	private String username;
-	private final Socket client;
-	private Player player;
-	private final Server server;
-	private GameThread game;
+public class Client extends Thread {
+
+	private final String host;
+	private final int port;
 
 	private ObjectInputStream in;
 	private ObjectOutputStream out;
 
-	public ClientThread(final Socket client, final Server server) {
+	public Client(final String host, final int port) {
 		super();
-		this.client = client;
-		this.server = server;
+		this.host = host;
+		this.port = port;
 		in = null;
 		out = null;
 	}
@@ -29,8 +26,9 @@ public class ClientThread extends Thread {
 	@Override
 	public void run() {
 		try (
+			Socket client = new Socket(host, port);
 			ObjectInputStream  in  = new ObjectInputStream(client.getInputStream());
-			ObjectOutputStream out = new ObjectOutputStream(client.getOutputStream());
+			ObjectOutputStream out = new ObjectOutputStream(client.getOutputStream())
 		) {
 			this.in = in;
 			this.out = out;
