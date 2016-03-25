87b9363d83d04a39426176d24b12abd2b672ec8f
Antonin Décimo <antonin.decimo@gmail.com>
2016-03-25 12:31:06 +0100

    Remove logging from main

--- src/main/java/org/copinf/cc/Main.java
+++ src/main/java/org/copinf/cc/Main.java
@@ -2,9 +2,6 @@ package org.copinf.cc;
 
 import org.copinf.cc.controller.MainController;
 
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
 import javax.swing.SwingUtilities;
 
 /**
@@ -20,10 +17,6 @@ public final class Main {
 	 * @param args arguments
 	 */
 	public static void main(final String[] args) {
-
-		Logger logger = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
-		logger.setLevel(Level.ALL);
-
 		SwingUtilities.invokeLater(new Runnable() {
 			public void run() {
 				new MainController().start();
